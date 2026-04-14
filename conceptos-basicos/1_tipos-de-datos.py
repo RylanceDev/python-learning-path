01d6e786b60b10bdab3ee9138ac0e90408509ae4
RylanceDev <safedaner@gmail.com>
2026-04-14 05:33:15 +0000

    Add files via upload

---
+++ conceptos-basicos/1_tipos-de-datos.py
@@ -0,0 +1,38 @@
+#Exploración de tipos de datos
+#Dev: Rylance
+
+# 1. Strings simples y dobles (Línea única)
+"Hola mundo"
+'Bienvenidos'
+
+# 1.1 Docstrings (Strings de múltiples líneas para datos estructurados)
+"""Tus datos son:
+        nombre : Daniel
+        apellido : Llanos"""
+
+# 1.2 Alternativa para bloques de texto
+'''
+ESTADO DEL JUGADOR:
+    ID      : #001
+    Alias   : Vantix
+    Nivel   : 1 (Basics)
+    Status  : Online
+'''
+
+# 2. int (entero)
+edad = 25               # int (positivo)
+temperatura = -10       # int (negativo)
+cantidad_clases = 0     # int (cero)
+
+# 3. float (decimal)
+precio = 19.99              # float (con decimales)
+gravedad = 9.8              # float (constante física)
+saldo = -150.50             # float (saldo negativo)
+entero_como_float = 40.0    # float (aunque el valor sea entero, el .0 lo cambia)
+
+# 4. bool (booleano)
+es_estudiante = True    # bool (Verdadero)
+tiene_deuda = False     # bool (Falso)
+
+# Nota: Estos bloques no se mostrarán en consola a menos que uses print()
+print(saldo)        # Muestra el valor de la variable 'saldo' en la terminal (-150.5)
